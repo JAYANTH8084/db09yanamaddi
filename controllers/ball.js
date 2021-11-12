7289438494948e36f73296755dec105ae6d2c845
Jayanth Yanamaddi <89415953+JAYANTH8084@users.noreply.github.com>
2021-11-12 02:40:33 -0600

    committed

--- controllers/ball.js
+++ controllers/ball.js
@@ -57,8 +57,24 @@ exports.ball_delete = function(req, res) {
 }; 
  
 // Handle ball update form on PUT. 
-exports.ball_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: ball update PUT' + req.params.id); 
+exports.ball_update_put =async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await ball.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.color)  
+               toUpdate.color = req.body.color; 
+        if(req.body.price) toUpdate.price = req.body.price; 
+        if(req.body.weight) toUpdate.weight = req.body.weight; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
 
 
