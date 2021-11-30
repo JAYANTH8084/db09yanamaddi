b984ac5a3136b0f607d98de6c8a46f9141ab5ea2
Jayanth Yanamaddi <89415953+JAYANTH8084@users.noreply.github.com>
2021-11-30 00:04:56 -0600

    Added bonus

--- models/ball.js
+++ models/ball.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose") 
 const ballSchema = mongoose.Schema({ 
- color: String, 
+ color: {type:String, required:true}, 
  price: { type: Number, min: 5, max: 60, default: 0 }, 
  weight: { type: Number, min: 1, max: 60, default: 0 } 
 }) 
--- routes/ball.js
+++ routes/ball.js
@@ -29,9 +29,9 @@ router.get('/', ball_controlers.ball_view_all_Page );
 /* GET detail ball page */
 router.get('/detail', ball_controlers.ball_view_one_Page);
 /* GET create ball page */
-router.get('/create', ball_controlers.ball_create_Page);
+router.get('/create',secured, ball_controlers.ball_create_Page);
 /* GET update ball page */
 router.get('/update',secured, ball_controlers.ball_update_Page);
 /* GET delete ball page */
-router.get('/delete', ball_controlers.ball_delete_Page);
+router.get('/delete',secured, ball_controlers.ball_delete_Page);
 module.exports = router;
